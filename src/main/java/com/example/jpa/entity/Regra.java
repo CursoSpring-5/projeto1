e6a80697a2ec46771232d3c4f0542d9e76f2a111
Anderson <andersoncc@disoft.com.br>
2018-06-11 08:42:11 -0300

    first commit

---
+++ src/main/java/com/example/jpa/entity/Regra.java
@@ -0,0 +1,30 @@
+package com.example.jpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Regra {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private Long id;
+	private String name;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
